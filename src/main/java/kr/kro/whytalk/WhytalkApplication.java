480107e189b3ad43301997a16afbe777330aeeef
Kim Hyeonseok <kim010322@gmail.com>
2022-08-06 20:23:50 +0900

    initial commit

---
+++ src/main/java/kr/kro/whytalk/WhytalkApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.whytalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhytalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhytalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/kro/whytalk/WhytalkApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kro.whytalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhytalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
