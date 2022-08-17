d91b76c9874d7f4980b9edca49be651396e0ec83
hyoju <rlagywn815@naver.com>
2022-08-18 08:26:35 +0900

    자동 빌드/배포

---
+++ src/main/java/kr/kro/whytalk/api/WhytalkBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.whytalk.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhytalkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhytalkBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/kr/kro/whytalk/api/model/Teacher.java
@@ -0,0 +1,8 @@
+package kr.kro.whytalk.api.model;
+
+public class Teacher {
+    private Long id;
+    private String username;
+    private String password;
+
+}
